bec93f7cb4bd7282413325e7a7791481cccc14df
GMIELAK <147776706+GMIELAK@users.noreply.github.com>
2026-04-12 20:40:47 -0500

    Add threaded send/receive for async messaging and multi-client support

--- client.py
+++ client.py
@@ -1,29 +1,58 @@
 import socket
+import threading
 
 ## This Program acts as the Client for a "chat" room for the CSCE 463 Project 3/4
 
- #Client TCP Socket
 def client_connection(name, port):
-    client_socket = socket.socket() 
+    client_socket = socket.socket()
     client_socket.connect((name, port))
     return client_socket
 
-#client sending
-def client_send(socket):
+
+def receive_messages(sock):
+    """Continuously listens for incoming messages from the server."""
+    try:
         while True:
-            message = socket.recv(1024).decode()
-            print("Message recieved from server =", message)
-            client_input = input("Send message back to the server:")
-            socket.send(client_input.encode())
+            message = sock.recv(1024).decode()
+            if not message:
+                break
+            print(f"\n{message}")
+    except Exception as e:
+        print("Connection lost:", e)
 
 
-#information
+def send_messages(sock, client_name):
+    """Continuously waits for user input and sends it to the server."""
+    try:
+        while True:
+            client_input = input(f"{client_name}: ")
+            sock.send(f"{client_name}: {client_input}".encode())
+    except Exception as e:
+        print("An error has occured:", e)
+
+
+def client_send(sock, client_name):
+    # Exchange names with server
+    sock.send(client_name.encode())
+    server_name = sock.recv(1024).decode()
+    print(f"Connected to server: {server_name}")
+    print("You can now send messages freely.\n")
+
+    # Spin up one thread for receiving, one for sending
+    recv_thread = threading.Thread(target=receive_messages, args=(sock,))
+    send_thread = threading.Thread(target=send_messages, args=(sock, client_name))
+    recv_thread.daemon = True
+    send_thread.daemon = True
+    recv_thread.start()
+    send_thread.start()
+
+    # Keep main thread alive while the two above run
+    recv_thread.join()
+
+
+# Information
 name = input('Enter Server IP address: ')
+client_name = input('Enter your name: ')
 port = 8080
 client_socket = client_connection(name, port)
-client_send(client_socket)
-
-
-
-
-
+client_send(client_socket, client_name)
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import threading
 
 ## This Program acts as the Server for a "chat" room for the CSCE 463 Project 3/4
 
-#setting up the chat room
+# Setting up the chat room
 chat_socket = socket.socket()
 chat_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 host_name = socket.gethostname()
@@ -12,39 +12,65 @@ port = 8080
 chat_socket.bind((host_name, port))
 
 
+def receive_messages(connection, client_name):
+    """Continuously listens for incoming messages from the client."""
+    try:
+        while True:
+            message = connection.recv(1024).decode()
+            if not message:
+                break
+            print(f"\n{message}")
+    except Exception as e:
+        print("Connection lost:", e)
+
+
+def send_messages(connection, server_name):
+    """Continuously waits for server operator input and sends it to the client."""
+    try:
+        while True:
+            my_input = input(f"{server_name}: ")
+            connection.send(f"{server_name}: {my_input}".encode())
+    except Exception as e:
+        print("An error has occured:", e)
+
+
 def talk_with_client(connection, address):
-            try:
-                print("Recieved connection from", address[0])
-                while True:
-                    my_input = input("Send message:")
-                    if("Recieved" not in my_input):
-                        connection.send(my_input.encode())
-                        result = connection.recv(1024).decode()
-                        print("Result equals:", result)
-            except Exception as e:
-                print("An error has occured:", e)
-        
+    try:
+        print("Recieved connection from", address[0])
+
+        # Exchange names
+        client_name = connection.recv(1024).decode()
+        print(client_name, "has connected.")
+        server_name = input("Enter your name: ")
+        connection.send(server_name.encode())
+
+        print("You can now send messages freely.\n")
+
+        # Spin up one thread for receiving, one for sending
+        recv_thread = threading.Thread(target=receive_messages, args=(connection, client_name))
+        send_thread = threading.Thread(target=send_messages, args=(connection, server_name))
+        recv_thread.daemon = True
+        send_thread.daemon = True
+        recv_thread.start()
+        send_thread.start()
+
+        # Keep this thread alive while the two above run
+        recv_thread.join()
+
+    except Exception as e:
+        print("An error has occured:", e)
+
 
 try:
     print("Welcome to Chat\n")
-
     print("Binding was successfull!")
-    print("Your IP = ", s_ip)
+    print("Your IP =", s_ip)
     chat_socket.listen(5)
 
-    #While loop so the chat room will keep making threads for new acceptances
     while True:
         connection, add = chat_socket.accept()
-        new_connection = threading.Thread(target=talk_with_client, args= (connection, add))
+        new_connection = threading.Thread(target=talk_with_client, args=(connection, add))
         new_connection.start()
 
 except Exception as e:
     print("An error has occured:", e)
-
-
-
-   
-
-
-
-
